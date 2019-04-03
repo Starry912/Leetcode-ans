0a9df108e5d045017da14bfcbef4e7b628d687fc
Yan Xinxing <1047043242@qq.com>
2019-04-03 16:05:43 +0800

    Update 在 D 天内送达包裹的能力.cpp
     https://leetcode-cn.com/contest/weekly-contest-128/problems/capacity-to-ship-packages-within-d-days/

--- 在 D 天内送达包裹的能力.cpp
+++ 在 D 天内送达包裹的能力.cpp
@@ -1,5 +1,4 @@
-1011 https://leetcode-cn.com/contest/weekly-contest-128/problems/capacity-to-ship-packages-within-d-days/
-传送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
+/*1011：传送带上的包裹必须在 D 天内从一个港口运送到另一个港口。
 传送带上的第 i 个包裹的重量为 weights[i]。每一天，我们都会按给出重量的顺序往传送带上装载包裹。我们装载的重量不会超过船的最大运载重量。
 返回能在 D 天内将传送带上的所有包裹送达的船的最低运载能力。
 
@@ -16,6 +15,7 @@
 
 思路：
 二分答案W_min
+*/
 
 class Solution {
 public:
